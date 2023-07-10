702b37f396a0b9c73467a16666c70a018a154f3e
Prosecutor1x <krishnarjunnanda@gmail.com>
2023-07-10 12:35:07 +0530

    Changes

--- context/userContext.tsx
+++ context/userContext.tsx
@@ -44,10 +44,10 @@ export function UserContextProvider({ children }: any) {
   }, []);
 
   const getUserData = useCallback(async () => {
-    setLoading(false);
+    
     let mobileNumber = jwtToken && (decodeJWT(jwtToken) as string);
     if (jwtToken !== "") {
-      const data = await checkUser(mobileNumber as string);
+      const data = await checkUser(mobileNumber?.substring(3) as string);
       setUser({ ...data, UserId: data._id });
     }
     setLoading(false);
